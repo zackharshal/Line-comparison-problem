fc95beed0892b75b3d304de9a51f5af0574e00ea
zackharshal <harshalgotarne25@gmail.com>
2024-06-11 10:38:24 +0530

    Compare 2 Lines
    Calculated the length of two lines, and checked if there lengths are equal or not and displaying it

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison computation problem");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the coordinates for 1st line:");
         System.out.println("Enter the coordinates of 1st point");
         System.out.print("Enter the x coordinate: ");
         int x1 = scanner.nextInt();
@@ -17,9 +18,27 @@ public class Main {
         int x2 = scanner.nextInt();
         System.out.print("Enter the y coordinate: ");
         int y2 = scanner.nextInt();
-        float lengthOfLine = (float) Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2 -y1), 2));
-        System.out.printf("Length of the line is %.2f" , lengthOfLine);
+        float lengthOfLine1 = (float) Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2 -y1), 2));
 
+        System.out.println("Enter the coordinates for 2nd line:");
+        System.out.println("Enter the coordinates of 1st point");
+        System.out.print("Enter the x coordinate: ");
+        int x3 = scanner.nextInt();
+        System.out.print("Enter the y coordinate: ");
+        int y3 = scanner.nextInt();
+        System.out.println("Enter the coordinates of 2nd point");
+        System.out.print("Enter the x coordinate: ");
+        int x4 = scanner.nextInt();
+        System.out.print("Enter the y coordinate: ");
+        int y4 = scanner.nextInt();
+        float lengthOfLine2 = (float) Math.sqrt(Math.pow((x4-x3), 2) + Math.pow((y4 -y3), 2));
+
+
+        if(lengthOfLine1 == lengthOfLine2){
+            System.out.println("Length of line 1 and line 2 are equal.");
+        }else{
+            System.out.println("Length of line 1 and line 2 are not equal.");
+        }
         
         scanner.close();
     }
